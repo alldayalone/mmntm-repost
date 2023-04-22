6f0fd25691b93e5ba0573693dc9cabaae4d21140
Pavel Kalugin <p.kalugin@paralect.com>
2023-04-22 20:17:00 +0300

    feat: v1

---
+++ README.md
@@ -0,0 +1,14 @@
+# mmntm-report
+chrome extension to push tweet or linkedin post to momentum feed
+
+# how to install
+1. clone repo
+2. go to chrome://extensions
+3. turn on "Developer mode"
+4. press "Load unpacked" and select `src` folder
+5. (optionally) pin the extension
+
+# build
+```bash
+zip -r packed.zip src
+```
---
+++ src/scripts/background.js
@@ -0,0 +1,85 @@
+chrome.action.onClicked.addListener(tab => {
+  chrome.tabs.create({
+    url: "https://app.mmntm.build/write",
+  }, (mmntmTab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: mmntmTab.id },
+      func: run,
+      args: [tab.url]
+    });
+  });
+});
+
+function run(repostLink) {
+  const selectors = {
+    submitUrlTab: () => document.querySelectorAll('button.tab')?.[1],
+    linkInput: () => document.querySelector('input'),
+    setLinkButton: () =>  document.querySelector('button[class="mt-4"]'),
+  }
+  
+  const events = {
+    click: (element) => element.dispatchEvent(new Event('click')),
+    type: (element, text) => {
+      const lastValue = element.value;
+      element.value = text
+      const event = new Event("input", { bubbles: true });
+      const tracker = element._valueTracker;
+      if (tracker) {
+        tracker.setValue(lastValue);
+      }
+      element.dispatchEvent(event);
+    }
+  }
+
+  function openSubmitUrlTab() {
+    const tab = selectors.submitUrlTab();
+
+    if (!tab) return false;
+
+    events.click(tab);
+    
+    return true;
+  }
+
+  function setTwitterUrl() {
+    const input = selectors.linkInput();
+
+    if (!input) return false;
+
+    events.type(input, repostLink);
+
+    return true;
+  }
+
+  function clickSetUrl() {
+    const setUrlBtn = selectors.setLinkButton();
+    
+    if (!setUrlBtn) return false;
+    
+    events.click(setUrlBtn);
+
+    return true;
+  }
+
+  const pipeline = [
+    () => true,
+    openSubmitUrlTab,
+    setTwitterUrl,
+    clickSetUrl
+  ];
+
+  function process() {
+    if (!pipeline.length) return;
+
+    const nextStep = pipeline[0];
+    const result = nextStep();
+
+    if (result) pipeline.shift();
+
+    setTimeout(process, 100);
+  }
+
+  process();
+}
+
+
